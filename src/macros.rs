378cc4fa5ff09cc5872fefb14b7622805c808a26
Philipp Schuster <phip1611@gmail.com>
2019-07-18 21:57:21 +0200

    malloc_no_conflict-Macro-update to generalize stmt und block-calls

--- src/macros.rs
+++ src/macros.rs
@@ -4,18 +4,21 @@
 /// 2) we have initialization-code that needs mallocs/frees and we don't want to log these calls
 #[macro_export]
 macro_rules! malloc_no_conflict {
-    ($code: stmt) => {{
+    ($code: block) => {
+        crate::internal_malloc_no_conflict!($code);
+    };
+    ($code: stmt) => {
+        // making Statement to Block
+        crate::internal_malloc_no_conflict!({$code;});
+    };
+}
+#[macro_export]
+macro_rules! internal_malloc_no_conflict {
+    ($code: block) => {
         if !endless_recur_protection::get_return_immediately() {
             endless_recur_protection::enable_return_immediately();
             $code;
             endless_recur_protection::disable_return_immediately();
         }
-    }};
-    ($code: block) => {{
-        if !endless_recur_protection::get_return_immediately() {
-            endless_recur_protection::enable_return_immediately();
-            $code;
-            endless_recur_protection::disable_return_immediately();
-        }
-    }}
+    }
 }
